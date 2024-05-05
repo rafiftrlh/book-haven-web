8b700e95edc332a018edec0671f7cfc68f6db251
rafiftrlh <rafifitro89@gmail.com>
2024-05-05 21:26:50 +0700

    create: migration, controller, model notification

---
+++ database/migrations/2024_05_05_141428_create_notifications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->text('message');
+            $table->enum('status', ['Read', 'Unread']);
+            $table->string('notification_type', 50);
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Define foreign key constraints
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
