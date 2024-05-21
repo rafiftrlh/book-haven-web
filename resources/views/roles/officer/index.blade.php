4511d06a4dd745ae3f93a40036458e2b43e021ed
ariekas <140879155+ariekas@users.noreply.github.com>
2024-05-21 16:50:51 +0700

    push projecta

--- resources/views/roles/officer/index.blade.php
+++ resources/views/roles/officer/index.blade.php
@@ -13,5 +13,9 @@
         @include('roles.officer.__home_officer')
     @elseif (request()->is('officer/add-category'))
         @include('roles.officer.__add_category')
+    @elseif (request()->is('officer/confirm_peminjaman'))
+        @include('roles.officer.__confirm_peminjaman')
+    @elseif (request()->is('officer/add_author'))
+        @include('roles.officer.__add_author')
     @endif
 @endsection
