6a5f8d8ad30baf9acb1dd8fd94658a6823dbc0f5
rafiftrlh <rafifitro89@gmail.com>
2024-05-30 17:55:04 +0700

    feat: fine data on the admin side based on the fine condition.

---
+++ resources/views/roles/admin/inner_page/__fine_late_and_broken_page.blade.php
@@ -0,0 +1,105 @@
+<div class="container-fluid p-4">
+    <a href="{{ route('admin.fines') }}" class="d-flex align-items-center gap-2 py-2 px-3 "
+        style="width: fit-content; border: 1px solid gray; border-radius: 100px;">
+        <svg style="rotate: -90deg" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32">
+            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"
+                d="m6 10l10-8l10 8M16 2v28" />
+        </svg>
+        <span style="font-weight: 700; font-size: 12px;">Back</span>
+    </a>
+    <div class="card mb-4 mt-4">
+        <div class="card-header pb-0 d-flex gap-1">
+            <h6>Late And Broken Fines</h6>
+            <span class="text-primary" style="font-size: 14px; font-weight: 700; margin-right: 2px; margin-top: 2px;">
+                @if ($totalLateAndBrokenFine > 99)
+                    99+
+                @else
+                    {{ $totalLateAndBrokenFine }}
+                @endif
+            </span>
+        </div>
+        @if ($totalLateAndBrokenFine == 0)
+            <div class="card-body px-0 pt-0 pb-4">
+                <p class="h4 text-secondary" style="text-align: center">
+                    No Late And Broken Fines
+                </p>
+            </div>
+        @else
+            <div class="card-body px-0 pt-0 pb-2">
+                <div class="table-responsive p-0">
+                    <table class="table align-items-center mb-0">
+                        <thead>
+                            <tr>
+                                <th
+                                    class="text-uppercase text-secondary text-xxs align-middle font-weight-bolder opacity-7">
+                                    Borrow Id
+                                </th>
+                                <th
+                                    class="text-uppercase text-secondary text-xxs align-middle font-weight-bolder opacity-7">
+                                    Book Code
+                                </th>
+                                <th
+                                    class="text-uppercase text-secondary text-xxs align-middle font-weight-bolder opacity-7">
+                                    Username
+                                </th>
+                                <th
+                                    class="text-uppercase text-secondary text-xxs align-middle font-weight-bolder opacity-7">
+                                    Borrow Date
+                                </th>
+                                <th
+                                    class="text-uppercase text-secondary text-xxs align-middle font-weight-bolder opacity-7">
+                                    Due Date
+                                </th>
+                                <th
+                                    class="text-uppercase text-secondary text-xxs align-middle font-weight-bolder opacity-7">
+                                    Return Date
+                                </th>
+                                <th
+                                    class="text-uppercase text-secondary text-xxs align-middle font-weight-bolder opacity-7">
+                                    Fines
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody id="req-approve-table-body">
+                            @foreach ($lateAndBrokenFines as $lateAndBrokenFine)
+                                <tr>
+                                    <td>
+                                        <p class="text-xs text-secondary mb-0 px-3" style="text-transform: uppercase;">
+                                            {{ $lateAndBrokenFine->borrowing_id }}</p>
+                                    </td>
+                                    <td>
+                                        <p class="text-xs text-secondary mb-0 px-3" style="text-transform: uppercase;">
+                                            {{ $lateAndBrokenFine->borrowing->books->book_code }}</p>
+                                    </td>
+                                    <td>
+                                        <p class="text-xs text-secondary mb-0 px-3">
+                                            &commat;{{ $lateAndBrokenFine->borrowing->users->username }}</p>
+                                    </td>
+                                    <td>
+                                        <p class="text-xs text-secondary mb-0 px-3">
+                                            {{ $lateAndBrokenFine->borrowing->borrow_date }}
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p class="text-xs text-secondary mb-0 px-3">
+                                            {{ $lateAndBrokenFine->borrowing->due_date }}
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p class="text-xs text-secondary mb-0 px-3">
+                                            {{ $lateAndBrokenFine->borrowing->return_date }}
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p class="text-xs text-secondary mb-0 px-3">{{ $lateAndBrokenFine->price }}
+                                        </p>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        @endif
+    </div>
+</div>
